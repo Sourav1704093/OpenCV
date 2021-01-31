84ffd30b8f6566ae3a9404a5dca6efef6eef332c
Sherlocked <42141718+Sourav1704093@users.noreply.github.com>
2021-01-31 18:42:19 +0600

    Update ScreenRecorder.py

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -1,22 +1,21 @@
-import cv2 #OpenCV
 import pyautogui
-import numpy as np
+import cv2 as cv ,numpy as np
 
+codec = cv.VideoWriter_fourcc(*"MPEG")
+output = cv.VideoWriter("Recorded.mp4", codec , 60, (1920, 1080)) 
 
-codec = cv2.VideoWriter_fourcc(*"MPEG")
-output = cv2.VideoWriter("Recorded.mp4", codec , 60, (1920, 1080)) 
-
-cv2.namedWindow("Recording", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Recording", 480, 270) 
+cv.namedWindow("Recording", cv.WINDOW_NORMAL)
+cv.resizeWindow("Recording", 480, 270) 
 
 
 while True:
     img = pyautogui.screenshot() 
     frame = np.array(img) 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB) 
     output.write(frame) 
-    cv2.imshow('Recording', frame) 
-    if cv2.waitKey(1) == ord('q'): 
+    cv.imshow('Recording', frame) 
+    if cv.waitKey(1) == ord('q'): 
         break
 output.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
+
